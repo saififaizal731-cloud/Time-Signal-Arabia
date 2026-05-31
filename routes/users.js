e5b08e4cf7e8ebeecdcd565ca84d3462fcb850fe
saififaizal731-cloud <saififaizal731@gmail.com>
2026-05-31 17:41:16 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Ensure this file exists in your models folder
-
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find(); // This queries MongoDB
-        res.json(users); // This sends the data as JSON to your browser
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
+router.get('/', (req, res) => res.send('Users route working')); // Change 'Users' to 'Cart' or 'Orders' for those files
 module.exports = router;
